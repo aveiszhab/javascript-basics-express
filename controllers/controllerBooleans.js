608af3dc81b240a5d49d8e2dd1e68148a7ace0f3
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-05-01 15:32:23 +0100

    booleans done

--- controllers/controllerBooleans.js
+++ controllers/controllerBooleans.js
@@ -1,3 +1,29 @@
-exports.hello = (req, res) => {
-  res.send('Hello, world!');
+const { negate, truthiness, isOdd, startsWith } = require('../src/lib/booleans');
+
+exports.booleansNegate = (req, res) => {
+  const a = req.body.value;
+  return res.status(200).json({ result: negate(a) });
+};
+
+exports.boolensTruthiness = (req, res) => {
+  const a = req.body.value;
+  return res.status(200).json({ result: truthiness(a) });
+};
+
+exports.booleansIsOdd = (req, res) => {
+  const a = parseInt(req.params.number, 10);
+  if (Number.isNaN(a)) {
+    return res.status(400).send({ error: 'Parameter must be a number.' });
+  }
+  return res.status(200).json({ result: isOdd(a) });
+};
+
+exports.booleanStartsWith = (req, res) => {
+  const char = req.params.character;
+  const strg = req.params.string;
+
+  if (char.length > 1) {
+    return res.status(400).send({ error: 'Parameter "character" must be a single character.' });
+  }
+  return res.status(200).json({ result: startsWith(char, strg) });
 };
--- routes/routeBooleans.js
+++ routes/routeBooleans.js
@@ -3,6 +3,9 @@ const booleansController = require('../controllers/controllerBooleans');
 
 const router = express.Router();
 
-router.get('/', booleansController.hello);
+router.post('/negate', booleansController.booleansNegate);
+router.post('/truthiness', booleansController.boolensTruthiness);
+router.get('/is-odd/:number', booleansController.booleansIsOdd);
+router.get('/:string/starts-with/:character', booleansController.booleanStartsWith);
 
 module.exports = router;
